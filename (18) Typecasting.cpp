a464f116bb616517fc6724ee7bd25ae5b3959b5e
md santo <140914322+santosarkar@users.noreply.github.com>
2024-10-14 11:42:59 +0600

    Create (18) Typecasting.cpp

---
+++ (18) Typecasting.cpp
@@ -0,0 +1,9 @@
+#include <iostream> // Including the standard input-output stream
+using namespace std; // Using the standard namespace
+
+int main() {
+    int a = 5; // Initializing an integer variable
+    double b = (double)a / 2; // Explicit typecasting from int to double
+    cout << "Result: " << b << endl; // Output the result
+    return 0; // Returning 0 to indicate successful execution
+}
